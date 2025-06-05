62fa941a95628ddb3cac381e70b39b4fa116e0ef
Kumar Gaurav <76562785+krgarav@users.noreply.github.com>
2025-06-05 14:35:17 +0530

    Fixed Linked Modal

--- src/modals/LinkModal/LinkModal.js
+++ src/modals/LinkModal/LinkModal.js
@@ -24,31 +24,39 @@ const LinkModal = (props) => {
   React.useEffect(() => {
     if (props.selectedCoordinates.length !== 0) {
       const linkedCoordinates = dataCtx.allTemplates[0][0].linkedCoordinates;
-
-      const filteredLinkedCoordinates = linkedCoordinates.filter((coordinate) =>
-        coordinate.fieldType.includes(props.fieldType)
-      );
-
-      const fields = props.selectedCoordinates.filter(
-        (field) => field.fieldType === props.fieldType
-      );
-
-      if (filteredLinkedCoordinates.length !== 0) {
-        const flattened = filteredLinkedCoordinates
-          .map((item) => item.fieldIndexes)
-          .flat();
-
-        // ✅ Assuming flattened indexes are based on `props.selectedCoordinates`
-        const result = fields.filter(
-          (field) =>
-            !flattened.includes(props.selectedCoordinates.indexOf(field))
+      console.log(linkedCoordinates);
+      if (linkedCoordinates) {
+        const filteredLinkedCoordinates = linkedCoordinates.filter(
+          (coordinate) => coordinate.fieldType.includes(props.fieldType)
         );
 
-        setFields(result);
-        return;
-      }
+        const fields = props.selectedCoordinates.filter(
+          (field) => field.fieldType === props.fieldType
+        );
 
-      setFields(fields);
+        if (filteredLinkedCoordinates.length !== 0) {
+          const flattened = filteredLinkedCoordinates
+            .map((item) => item.fieldIndexes)
+            .flat();
+
+          // ✅ Assuming flattened indexes are based on `props.selectedCoordinates`
+          const result = fields.filter(
+            (field) =>
+              !flattened.includes(props.selectedCoordinates.indexOf(field))
+          );
+
+          setFields(result);
+          return;
+        }
+      } else {
+        if (props.selectedCoordinates.length !== 0) {
+          const fields = props.selectedCoordinates.filter((field) => {
+            return field.fieldType === props.fieldType;
+          });
+          setFields(fields);
+        }
+      }
+      // setFields(fields);
     }
   }, [props.selectedCoordinates, props.fieldType]);
 
