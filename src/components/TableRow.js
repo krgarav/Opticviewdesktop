c482e8bd38ad2550f97d41550fa3486c3bcd544e
krgarav <krgarav@gmail.com>
2025-06-28 22:21:06 +0530

    handled tablerow

--- src/components/TableRow.js
+++ src/components/TableRow.js
@@ -1,99 +1,171 @@
-import React from "react";
-import IconButton from "@mui/material/IconButton";
-import ArrowCircleUpIcon from "@mui/icons-material/ArrowCircleUp";
-import ArrowCircleDownIcon from "@mui/icons-material/ArrowCircleDown";
+import React, {
+  useCallback,
+  useContext,
+  useEffect,
+  useMemo,
+  useState,
+} from "react";
 import {
-  UncontrolledDropdown,
-  DropdownToggle,
   DropdownMenu,
   DropdownItem,
+  UncontrolledDropdown,
+  DropdownToggle,
+  Table,
 } from "reactstrap";
+import ArrowCircleUpIcon from "@mui/icons-material/ArrowCircleUp";
+import ArrowCircleDownIcon from "@mui/icons-material/ArrowCircleDown";
+import IconButton from "@mui/material/IconButton";
 
 const TableRow = ({
-  fieldData,
   type,
-  handleSort,
-  editHandler,
-  deleteHander,
+  fieldData,
   selectedItems,
+  handleSort = () => {},
+  editHandler = () => {},
+  deleteHander = () => {},
   handleCheckboxChange,
 }) => {
-  return (
-    <>
-      {fieldData.map((item, i) => {
-        const uniqueId = `${item.name}-${i}`;
-        const handleRowClick = () => {
-          handleCheckboxChange(uniqueId);
-        };
-        return (
-          <tr key={uniqueId} onClick={handleRowClick}>
-            <td>
-              <input
-                type="checkbox"
-                checked={selectedItems.includes(uniqueId)}
-                onChange={() => handleCheckboxChange(uniqueId)}
-              />
-            </td>
-            <td>{i + 1}</td>
-            <td>{item.name}</td>
-            <td>{item.fieldType}</td>
-            <td>
-              <IconButton
-                onClick={() => handleSort(type, moveItemUp(fieldData, i))}
-                aria-label="move up"
-              >
-                <ArrowCircleUpIcon fontSize="inherit" />
-              </IconButton>
-              <IconButton
-                onClick={() => handleSort(type, moveItemDown(fieldData, i))}
-                aria-label="move down"
-              >
-                <ArrowCircleDownIcon fontSize="inherit" />
-              </IconButton>
-            </td>
-            <td>
-              <UncontrolledDropdown>
-                <DropdownToggle
-                  className="btn-icon-only"
-                  href="#pablo"
-                  role="button"
-                  size="sm"
-                  onClick={(e) => e.preventDefault()}
-                >
-                  <i className="fas fa-ellipsis-v" />
-                </DropdownToggle>
-                <DropdownMenu className="dropdown-menu-arrow" right>
-                  <DropdownItem onClick={() => editHandler(item, i)}>
-                    Edit
-                  </DropdownItem>
-                  <DropdownItem
-                    style={{ color: "red" }}
-                    onClick={() => deleteHander(item, i)}
-                  >
-                    Delete
-                  </DropdownItem>
-                </DropdownMenu>
-              </UncontrolledDropdown>
-            </td>
-          </tr>
-        );
-      })}
-    </>
-  );
-};
+  const [direction, setDirection] = useState(null);
+  const [fields, setFields] = useState(fieldData);
+  const [animatingIndex, setAnimatingIndex] = useState(null);
+  const setFieldsCallback = useCallback((data) => setFields(data), []);
 
-const moveItemUp = (data, index) => {
-  if (index === 0) return data;
-  const newData = [...data];
-  [newData[index], newData[index - 1]] = [newData[index - 1], newData[index]];
-  return newData;
-};
+  useEffect(() => {
+    setFields(fieldData);
+  }, [fieldData]);
+  console.log(fieldData);
 
-const moveItemDown = (data, index) => {
-  if (index === data.length - 1) return data;
-  const newData = [...data];
-  [newData[index], newData[index + 1]] = [newData[index + 1], newData[index]];
-  return newData;
+  useEffect(() => {
+    if (typeof handleSort === "function") {
+      handleSort(type, fields);
+    }
+  }, [fields, handleSort]);
+
+  const moveUp = (index) => {
+    if (index > 0) {
+      setAnimatingIndex(index);
+      setDirection("up");
+      setTimeout(() => {
+        const newFields = [...fields];
+        [newFields[index], newFields[index - 1]] = [
+          newFields[index - 1],
+          newFields[index],
+        ];
+        setFields(newFields);
+        setAnimatingIndex(null);
+        setDirection(null);
+        resetAnimation();
+      }, 300);
+    }
+  };
+
+  const moveDown = (index) => {
+    if (index < fields.length - 1) {
+      setAnimatingIndex(index);
+      setDirection("down");
+      setTimeout(() => {
+        const newFields = [...fields];
+        [newFields[index], newFields[index + 1]] = [
+          newFields[index + 1],
+          newFields[index],
+        ];
+        setFields(newFields);
+        setAnimatingIndex(null);
+        setDirection(null);
+        resetAnimation();
+      }, 300);
+    }
+  };
+
+  const resetAnimation = () => {
+    setAnimatingIndex(null);
+    setDirection(null);
+  };
+
+  return fields?.map((item, i) => {
+    const isAnimating = animatingIndex === i;
+    const slno = isAnimating ? (direction === "up" ? i + 1 : i - 1) : i + 1;
+    let fieldValue = type === "formField" ? "FF" : "QF";
+    if (type === "formField") {
+      fieldValue = "Fm_F";
+    } else if (type === "questionField") {
+      fieldValue = "Qn_F";
+    } else if (type === "skewField") {
+      fieldValue = "Sk_F";
+    } else {
+      fieldValue = "Id_F";
+    }
+    const uniqueId = `${item.name}-${i}`;
+    const isSelected = selectedItems.includes(uniqueId);
+    return (
+      <tr
+        key={i}
+        style={{
+          backgroundColor: isAnimating
+            ? direction === "up"
+              ? "#f0f0f0"
+              : "#d9d9d9"
+            : "transparent",
+          transition:
+            "background-color 0.3s ease-in-out, transform 0.3s ease-in-out",
+          transform: isAnimating
+            ? direction === "up"
+              ? "translateY(-50px)"
+              : "translateY(50px)"
+            : "none",
+        }}
+      >
+        <td onClick={(e) => e.stopPropagation()}>
+        <input
+          type="checkbox"
+          checked={isSelected}
+          onChange={() => handleCheckboxChange(uniqueId)}
+        />
+      </td>
+        <td> {fieldValue + slno}</td> {/* Serial number */}
+        <td>{item.name}</td>
+        <td>{item.fieldType}</td>
+        <td>
+          <IconButton onClick={() => moveUp(i)} aria-label="move up">
+            <ArrowCircleUpIcon fontSize="inherit" />
+          </IconButton>
+          <IconButton onClick={() => moveDown(i)} aria-label="move down">
+            <ArrowCircleDownIcon fontSize="inherit" />
+          </IconButton>
+        </td>
+        <td>
+          <UncontrolledDropdown>
+            <DropdownToggle
+              className="btn-icon-only "
+              href="#pablo"
+              role="button"
+              size="sm"
+              onClick={(e) => e.preventDefault()}
+            >
+              <i className="fas fa-ellipsis-v" />
+            </DropdownToggle>
+            <DropdownMenu className="dropdown-menu-arrow" right>
+              <DropdownItem
+                onClick={() => {
+                  editHandler(item, i);
+                }}
+              >
+                Edit
+              </DropdownItem>
+              <DropdownItem
+                onClick={() => {
+                  deleteHander(item, i);
+                }}
+                style={{ color: "red" }}
+              >
+                Delete
+              </DropdownItem>
+            </DropdownMenu>
+          </UncontrolledDropdown>
+        </td>
+      </tr>
+    );
+  });
 };
 
 export default React.memo(TableRow);
